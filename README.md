59070ce4833f13d9e6dcf800898c523b3d3f86f1
Adrián Norte <adrian@eyeos.com>
2017-02-06 22:20:06 +0100

    Adding a README file

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# VoteEuropa
+The idea behind this repo is to create a Voting platform tailored to our needs
--- src/server.js
+++ src/server.js
@@ -13,4 +13,5 @@
  GNU Affero General Public License for more details.
  You should have received a copy of the GNU Affero General Public License
  along with VoteEuropa. If not, see <http://www.gnu.org/licenses/>.
- */
+*/
+
